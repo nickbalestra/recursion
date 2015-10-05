b46cc083305be7c140c08c41e8a363632f960a4f
Nick Balestra <nick@mba.local>
2015-10-05 13:13:08 -0700

    Es6 implementation

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,32 @@
 
 // Inner recursive function implementation
 var getElementsByClassName = className => {
+  var elements = [];
+
+  var searchNodes = node => {
+    if (node.classList.contains(className)) {
+      elements.push(node);
+    }
+    
+    Array.from(node.children).forEach(searchNodes);
+  };
+
+  searchNodes(document.body);
+  return elements;
+};
+
+// Self invoking function implementation
+var getElementsByClassName = (className,  node = document.body) => {
+  var elements = [];
+
+  if (node.classList.contains(className)) {
+    elements.push(node);
+  }
+    
+  Array.from(node.children).forEach(child => {
+    elements = elements.concat(getElementsByClassName(className, child))
+  });
   
+  searchNodes(document.body);
+  return elements;
 };
