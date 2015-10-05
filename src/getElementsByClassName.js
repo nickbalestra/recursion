a4251eeb12565c6ca72c813960fabf60704f9fa2
Nick Balestra <nick@mba.local>
2015-10-05 11:08:01 -0700

    Initialized GIT

---
+++ src/getElementsByClassName.js
@@ -0,0 +1,9 @@
+// If life was easy, we could just do things the easy way:
+// var getElementsByClassName = function (className) {
+//   return document.getElementsByClassName(className);
+// };
+
+// Inner recursive function implementation
+var getElementsByClassName = className => {
+  
+};
